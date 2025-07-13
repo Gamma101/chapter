337739729c783302fe586df46bfcbde2077ab475
HowBoutDat47 <zaikinvladislav74@gmail.com>
2025-07-13 23:54:44 +0500

    Identity JWT create login plus swagger auth

--- backend/Controllers/AccountController.cs
+++ backend/Controllers/AccountController.cs
@@ -13,12 +13,32 @@ namespace backend.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid) { return BadRequest(ModelState); }
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+            if (user == null) { return Unauthorized("Invalid username!"); }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded) { return Unauthorized("Invalid username or password!"); }
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+                );
         }
 
+        
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
