298af5497831855c24ab4b5785e21e9aa1f18ef6
Gamma101 <danilolenberger@gmail.com>
2025-07-21 19:10:13 +0500

    styles: add skeleton to book info page

--- frontend/app/(protected)/book/[bookId]/page.tsx
+++ frontend/app/(protected)/book/[bookId]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import BookPageSkeleton from "@/components/BookPageSkeleton"
 import { BackendBook, Review } from "@/types/book"
 import axios from "axios"
 import Image from "next/image"
@@ -11,6 +12,7 @@ export default function BookPage() {
   const bookId = params.bookId as string
   const [bookInfo, setBookInfo] = useState<BackendBook | null>(null)
   const [reviews, setReviews] = useState<Review[] | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
 
   // Parse book information
   useEffect(() => {
@@ -20,9 +22,11 @@ export default function BookPage() {
         .then((data) => {
           console.log(data.data)
           setBookInfo(data.data)
+          setIsLoading(false)
         })
         .catch((error) => {
           console.log(error)
+          setIsLoading(false)
         })
     }
     parseBookInfo()
@@ -42,37 +46,43 @@ export default function BookPage() {
         })
     }
     parseBookReviews()
-  }, [])
+  }, [bookId])
 
   return (
-    <div className="flex flex-col">
-      {bookInfo && (
-        <div className="container flex gap-10">
-          <Image
-            alt="book"
-            src={bookInfo.thumbnailUrl}
-            width={300}
-            height={500}
-            unoptimized
-            className="rounded-sm"
-          />
-          <div className="w-[50%] flex flex-col gap-5">
-            <h1 className="text-3xl font-bold">{bookInfo.title}</h1>
-            <p>{bookInfo.description}</p>
-          </div>
-        </div>
-      )}
-      {reviews && (
-        <div className="">
-          {reviews.map((preview, key) => {
-            return (
-              <div key={key}>
-                <p>{preview.title}</p>
-                <p>{preview.content}</p>
-                <p>{preview.createdBy}</p>
+    <div className="flex items-center justify-center mt-5">
+      {isLoading ? (
+        <BookPageSkeleton />
+      ) : (
+        <div className="flex flex-col">
+          {bookInfo && (
+            <div className="container flex gap-10">
+              <Image
+                alt="book"
+                src={bookInfo.thumbnailUrl}
+                width={300}
+                height={500}
+                unoptimized
+                className="rounded-sm"
+              />
+              <div className="w-[50%] flex flex-col gap-5">
+                <h1 className="text-3xl font-bold">{bookInfo.title}</h1>
+                <p>{bookInfo.description}</p>
               </div>
-            )
-          })}
+            </div>
+          )}
+          {reviews && (
+            <div className="flex w-[80%] self-center bg-secondary rounded-lg p-10">
+              {reviews.map((preview, key) => {
+                return (
+                  <div key={key} className="">
+                    <p>{preview.title}</p>
+                    <p>{preview.content}</p>
+                    <p>{preview.createdBy}</p>
+                  </div>
+                )
+              })}
+            </div>
+          )}
         </div>
       )}
     </div>
